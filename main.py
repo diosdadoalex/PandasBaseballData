2c1a6c2f9a7a21bf307b46106cadae7be773337c
diosdadoalex <63257386+diosdadoalex@users.noreply.github.com>
2023-12-10 10:28:29 -0800

    added data visualizations code

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import pandas as pd
-
-# Datasets found on Kaggle at https://www.kaggle.com/datasets/open-source-sports/baseball-databank
+import matplotlib.pyplot as plt
+# Datasets found on Kaggle at https://www.kaggle.com/datasets/mathchi/hitters-baseball-data
 
 # Analyze the Average Batting Average for every year
 def batting_analysis():
-    batting = pd.read_csv("Batting.csv")
-    # Remove NA values from data
+    batting = pd.read_csv("Hitters.csv")
+
+    print(batting.head())
+    # Remove All Na values from data
     batting.dropna(inplace=True)
 
-    # Remove any player with no recorded AB's
-    for index in batting.index:
-        if batting.loc[index, "AB"] < 1 or batting.loc[index, "yearID"] < 1950:
-            batting.drop(index, inplace=True)
+    batting["Avg"] = batting["Hits"] / batting["AtBat"]
+    batting["CAvg"] = batting["CHits"] / batting["CAtBat"]
+    batting["AvgDiff"] = batting["Avg"] - batting["CAvg"]
+    print(batting.head())
 
-    batting["AVG"] = batting["H"] / batting["AB"]
+    # Average batting Avg for players in data
+    print(batting["CAvg"].mean())
 
+    # Find career average by years in league
+    #batting.plot.scatter(x="Years", y="CAvg", figsize=(20, 10));
+    #plt.show()
 
-    print(batting)
-
+    # Check for visual correlation between salary and amount of home runs hit
+    sort_by_salary = batting.sort_values(by="Salary")
+    sort_by_salary.plot.bar(x = "Salary", y="HmRun")
+    plt.show()
 
 if __name__ == "__main__":
     batting_analysis()
